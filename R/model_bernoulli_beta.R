3fb34295eac68574d19df5c2fafa9ba2a1d93590
Elio Bartoš <eliobartos@yahoo.com>
2020-06-08 09:21:52 +0200

    initial push

---
+++ R/model_bernoulli_beta.R
@@ -0,0 +1,30 @@
+#' Run bayesian beta - bernoulli model for estimating proportion of a bernoulli variable
+#'
+#' Runs a bayesian estimation of proportion using bernoulli distribution as likelihood and beta distribution as conjugate prior.
+#' Posterior distribution is beta distribution.
+#' Prior used is Beta(alpha + 1, beta + 1)
+#'
+#' @param alpha Parameter for prior distribution representing the number of success
+#' @param beta Parameter for prior distribution representing the number of fails
+#'
+#' @param success Number of success cases in your data
+#' @param n_sample Total number of cases in your data
+#' @param n_post Size of sample from posterior distribution
+#'
+#' @return Vector of samples from posterior distribution
+#'
+#' Posterior distribution is Beta(alpha + 1 + success, beta + 1 + total - success)
+#'
+#' @examples
+#' post = model_bernoulli_beta(0, 0, 20, 100) # No prior information, pror is uniform
+#' post2 = model_bernoulli_beta(5, 95, 3, 50) # Prior succes rate is around 5% with estimation strenght as it was estimated on a sample of 100
+#'
+#' mean(post)
+#' quantile(post, probs = c(0.05, 0.95)) # 90% highest density posterior interval
+#'
+#' mean(post2)
+#' quantile(post2, probs = c(0.05, 0.95))
+model_bernoulli_beta = function(alpha, beta, success, n_sample, n_post = 1e5) {
+  rbeta(n_post, alpha + 1 + success, beta + 1 + n_sample - success)
+}
+
---
+++ R/model_exponential_gamma.R
@@ -0,0 +1,32 @@
+#' Run bayesian exponential-gamma model for estimating non-zero positive variable
+#'
+#' Runs a bayesian estimation of non-zero positive variable using exponential distribution as likelihood and gamma distribution as conjugate prior.
+#' Posterior distribution is gamma distribution.
+#' Prior used is Gamma(shape, rate)
+#'
+#' Mean of prior distribution is shape/rate. Remember Exp(lambda) has mean of 1/lambda.
+#'
+#' @param shape Parameter for prior distribution representing the number of samples from before
+#' @param rate Parameter for prior distribution representing the sum of samples from before
+#'
+#' @param n_sample Total number of cases in your data
+#' @param sum_sample Number of success cases in your data
+#' @param n_post Size of sample from posterior distribution
+#'
+#' @return Vector of samples from posterior distribution
+#'
+#' Posterior distribution is Gamma(shape + n_sample, rate + sum_sample)
+#'
+#' @examples
+#' post = model_exponential_gamma(0, 0, 20, 100) # No prior information, pror is uniform
+#' post2 = model_exponential_gamma(5, 95, 3, 50) # Prior succes rate is around 5% with estimation strenght as it was estimated on a sample of 100
+#'
+#' mean(post)
+#' quantile(post, probs = c(0.05, 0.95)) # 90% highest density posterior interval
+#'
+#' mean(post2)
+#' quantile(post2, probs = c(0.05, 0.95))
+model_exponential_gamma = function(shape, rate, n_sample, sum_sample, n_post = 1e5) {
+  rgamma(sample_size, shape + n_sample, rate + sum_sample)
+}
+
